c7f0827a05852609da3d59fc078ae401fb65e814
Délio Cantarelli <delio@manifestohut.com.br>
2020-02-01 23:34:01 -0300

    making it work

--- Assets/Project/Scripts/Cards/ManaDisplay.cs
+++ Assets/Project/Scripts/Cards/ManaDisplay.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GGJ20.CardRules;
 using UnityEngine;
 // using UnityEngine.UI;
 using Zenject;
