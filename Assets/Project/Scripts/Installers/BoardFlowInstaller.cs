43bbc987e968444ba24f229da0aa3d7dd6bbac49
Antonio Harger Rivero <antonio.harger@gmail.com>
2020-02-02 00:09:54 -0300

    Fixed after merge

--- Assets/Project/Scripts/Installers/BoardFlowInstaller.cs
+++ Assets/Project/Scripts/Installers/BoardFlowInstaller.cs
@@ -6,8 +6,6 @@ using GGJ20.Target;
 namespace GGJ20.Installers {
     public class BoardFlowInstaller : MonoInstaller
     {
-        [SerializeField]
-        private Transform LogicObjectParent;
         public override void InstallBindings()
         {
             Container.Bind<TargetsManager>().FromNewComponentOnNewGameObject()
